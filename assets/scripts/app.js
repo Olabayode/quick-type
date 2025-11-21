fdf46602e71e9c9b9f212ce5d4df8207bd431e54
Emmanuel Olure <emmanuelolabayode@gmail.com>
2025-11-20 23:42:29 -0600

    initial game sound added

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,33 @@
 'use strict';
+
+const welcome = new Audio('./assets/media/welcome.mp3');
+welcome.muted = true;
+welcome.type = 'audio/mp3';
+
+const begin = new Audio('./assets/media/begin.mp3');
+begin.type = 'audio/mp3';
+begin.loop = true;
+
+//query selector
+const startBtn = document.querySelector('.start-button')
+
+//event listener
+window.addEventListener('load', () => {
+  welcome.play().catch(error=> {
+    console.log('Muted audio play prevented :', error)
+  })
+})
+
+window.addEventListener('click', () => {
+    welcome.muted = false;
+    welcome.play();
+}, {once: true});
+
+startBtn.addEventListener('click', ()=> {
+    welcome.pause();
+    welcome.currentTime = 0;
+
+    begin.play().catch(error => {
+        console.log('Start audio prevented:', error)
+    });
+})
