a54fc3010c4dc51e722495e3620b05f3c08990d9
LB <lboyko@student.mitt.ca>
2025-11-22 10:16:54 -0600

    Added starting timer

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -13,6 +13,8 @@ begin.loop = true;
 //query selector
 const startBtn = select('.start-button');
 const gameBox = select('.game-container');
+const startCount = select('.start-countdown');
+const userInput = getElement('user-input');
 
 //event listener
 listen('click', window, () => {
@@ -32,9 +34,30 @@ listen('click', startBtn, () => {
 
 //start function 
 
+
 function openGame() {
     startBtn.style.visibility = 'hidden';
     startBtn.style.opacity = '0';
-    gameBox.style.visibility = 'visible';
-    gameBox.style.opacity = '1';
+    setInterval(countdown, 1000);
+    setTimeout(() => {
+        gameBox.style.visibility = 'visible';
+        gameBox.style.opacity = '1';
+        userInput.focus();
+    }, 4000);
+}
+
+let counter = 3;
+
+function countdown() {
+    if (counter === 0) {
+        startCount.style.visibility = 'hidden';
+        startCount.style.opacity = '0';
+    } else {
+        startCount.style.visibility = 'visible';
+        startCount.style.opacity = '1';
+        startCount.innerHTML = `<h2>${counter}</h2>`;
+    }
+    if (counter >= 1) {
+        counter--;
+    }
 }
